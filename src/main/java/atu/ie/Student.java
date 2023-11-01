79b8372e908b77f511c0d3c513b07026a2d0b0d5
Brexo1 <146333829+Brexo1@users.noreply.github.com>
2023-11-01 21:35:47 +0000

    Update Student.java
    Added detailed comments and updated code

--- src/main/java/atu/ie/Student.java
+++ src/main/java/atu/ie/Student.java
@@ -1,35 +1,94 @@
-package atu.ie;
+package ie.atu;
 
 import java.util.Scanner;
 
 public class Student {
-    String studentName;
-    String studentEmail;
-    String studentCourse;
-    public Student(){
-        this.studentName = "";
-        this.studentEmail = "";
-        this.studentCourse = "";
-    }
+    // Define private fields to store student information
+    private String studentName;
+    private String studentEmail;
+    private String courseName;
+    private String studentAName;
+    private String studentAEmail;
+    private String courseAName;
+    private String studentCName;
+    private String studentCEmail;
+    private String courseCName;
 
-    public Student(String studentName, String studentEmail, String studentCourse){
+    // Setters to assign values to the fields for the first student
+    public void setStudentName(String studentName) {
         this.studentName = studentName;
-        this.studentEmail = studentEmail;
-        this.studentCourse = studentCourse;
     }
 
-    public void getUserInput(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter Your First Name:  ");
-        this.studentName = scanner.nextLine();
-        System.out.print("Enter Your Email:  ");
-        this.studentEmail = scanner.nextLine();
-        System.out.print("Enter Your Course:  ");
-        this.studentCourse = scanner.nextLine();
+    public void setStudentEmail(String studentEmail) {
+        this.studentEmail = studentEmail;
     }
-    public void displayInfo(){
-        System.out.println(" " + studentName);
-        System.out.println(" " + studentEmail);
-        System.out.println(" " + studentCourse);
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    // Getters to retrieve values from the fields for the first student
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public String getStudentEmail() {
+        return studentEmail;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    // Constructor with default values for the first student
+    public Student() {
+        this.studentName = "DefaultName";
+        this.studentEmail = "DefaultEmail@atu.ie";
+        this.courseName = "DefaultCourse";
+    }
+
+    // Constructor with a single argument for the second student
+    public Student(String studentAName) {
+        this.studentAName = studentAName;
+    }
+
+    // Setters and getters for the second student (studentAName)
+    public void setStudentAEmail(String studentAEmail) {
+        this.studentAEmail = studentAEmail;
+    }
+
+    public void setCourseAName(String courseAName) {
+        this.courseAName = courseAName;
+    }
+
+    public String getStudentAEmail() {
+        return studentAEmail;
+    }
+
+    public String getCourseAName() {
+        return courseAName;
+    }
+
+    // Constructor with three arguments for the third student
+    public Student(String studentCName, String studentCEmail, String courseCName) {
+        this.studentCName = studentCName;
+        this.studentCEmail = studentCEmail;
+        this.courseCName = courseCName;
+    }
+
+    // Method to display information for the first student
+    public void displayInfo() {
+        System.out.println("\nStudent Name: " + getStudentName() + "\nStudent Email: " + getStudentEmail() + "\nCourse Name: " + getCourseName());
+    }
+
+    // Method to display information for the second student
+    public void displayInfoA() {
+        System.out.println("\nStudent Name: " + studentAName + "\nStudent Email: " + getStudentAEmail() + "\nCourse Name: " + getCourseAName());
+    }
+
+    // Method to display information for the third student
+    public void displayInfoC() {
+        System.out.println("\nStudent Name: " + studentCName + "\nStudent Email: " + studentCEmail + "\nCourse Name: " + courseCName);
     }
 }
+
